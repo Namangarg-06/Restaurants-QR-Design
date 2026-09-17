bc59910f57d8469d4f5cf07603e8a0cbebd60916
namangarg-06 <namangarg620@gmail.com>
2026-09-17 13:41:48 +0530

    update

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ var RESTAURANT_CONFIG = {
     // 4. Instagram Link & Profile Handle
     instagramLink: "https://www.instagram.com/itzz._.naman?stkn=MXVmMTVva2hjM2Jzdw%3D%3D&utm_source=qr",
     instagramUsername: "@itzz._.naman",
-    instagramActionText: " baap ko follow kr kamine Instagram par",
+    instagramActionText: " baap ko follow kr kamine Instagram par jaldi se",
     instagramSubtext: "@itzz._.naman • Food, Reels & Updates",
 
     // 5. Contact & Location Details
